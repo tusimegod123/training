f9015146f7cc42c4f834fbb624bf489a9b0f75f7
Tusime Godwin <tusimegodwin@Tusimes-MacBook-Pro.local>
2021-03-04 18:59:02 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// var action = "You have clicked a submit button";
+//     document.getElementById("action")
+//     console.log(action);
+
+// function cancelButton() {
+//     console.log("You have clicked a Cancel button");
+// }
+// function getSelectValue(){
+//     var selectedValue = document.getElementById("list").value;
+//     console.log("you have selected" + " " + selectedValue + " " + " and " + action );
+
+// }
+var user_name;
+user_name = "Godw"
+document.write(user_name)
+
